dd4d193ab9da26efc12e6e7d87343b3abbf14d88
Pablo Pons <pablo.romeroponsfuster@gmail.com>
2024-06-19 19:28:19 +0200

    Añadida comprobacion de tipo de argumento e si tiene valor no numérico

---
+++ src/args.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/19 10:42:10 by paromero          #+#    #+#             */
+/*   Updated: 2024/06/19 10:42:10 by paromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+
+void	one_arg(char *array)
+{
+	int i;
+
+	i = 0;
+	while (array[i] != '\0')
+	{
+		if (array[i] < '0' || array[i] > '9')
+			ft_error();
+		i++;
+	} 
+}
+
+void	various_args(int ac, char **av)
+{
+	int	j;
+	int	i;
+
+	i = 1;
+	while (i < ac)
+    {
+        j = 0;
+        while (av[i][j] != '\0')
+        {
+            if (av[i][j] < '0' || av[i][j] > '9')
+                ft_error();
+            j++;
+        }
+		i++;
+    }
+}
+
+void check_args(int ac, char **av)
+{
+    if (ac == 2)
+		one_arg(av[1]);
+	else if (ac > 2)
+		various_args(ac, av);
+	else
+		exit(EXIT_SUCCESS);
+}
