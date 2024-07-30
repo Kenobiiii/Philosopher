429f99fc484f09ba09f11325d8a7285a3de915e7
Pablo Pons <pablo.romeroponsfuster@gmail.com>
2024-07-30 07:06:52 +0200

    Añadida func de lista enlazada
    añadido main de prueba
    cambiando check_args, various_args, one_arg para adaptarlo a las listas
    añadido ft_add y hasduplicates

--- src/args.c
+++ src/args.c
@@ -12,48 +12,165 @@
 
 #include "push_swap.h"
 
-void	one_arg(char *array)
-{
-	int	i;
+// void	one_arg(char *array)
+// {
+// 	int	i;
 
-	i = 0;
-	while (array[i] != '\0')
+// 	i = 0;
+// 	while (array[i] != '\0')
+// 	{
+// 		if (array[i] == ' ')
+// 			i++;
+// 		if (array[i] < '0' || array[i] > '9')
+// 			ft_error();
+// 		i++;
+// 	}
+// }
+
+// void	various_args(int ac, char **av)
+// {
+// 	int	j;1
+// 	int	i;
+
+// 	i = 1;
+// 	while (i < ac)
+// 	{
+// 		j = 0;
+// 		while (av[i][j] != '\0')
+// 		{
+// 			if (av[1][j] == ' ')
+// 				ft_error();
+// 			else if (av[i][j] < '0' || av[i][j] > '9')
+// 				ft_error();
+// 			j++;
+// 		}
+// 		i++;
+// 	}
+// }
+
+// void	check_args(int ac, char **av)
+// {
+// 	if (ac == 2)
+// 		one_arg(av[1]);
+// 	else if (ac > 2)
+// 		various_args(ac, av);
+// 	else
+// 		exit(EXIT_SUCCESS);
+// }
+// Función para verificar argumentos y crear la lista enlazada
+
+void ft_add(t_stack** head_ref, int new_value, int new_pos)
+{
+	t_stack* new_node = (t_stack*)malloc(sizeof(t_stack));
+	t_stack* last = *head_ref;
+
+	new_node->value = new_value;
+	new_node->pos = new_pos;
+	new_node->index = 0;
+	new_node->target_pos = 0;
+	new_node->cost_a = 0;
+	new_node->cost_b = 0;
+	new_node->next = NULL;
+
+	if (*head_ref == NULL) {
+		*head_ref = new_node;
+		return;
+	}
+	int index = 1;
+	while (last->next != NULL) {
+		last = last->next;
+		index++;
+	}
+	new_node->index = index;
+	last->next = new_node;
+}
+
+int hasDuplicates(t_stack* head) {
+	t_stack* current = head;
+	t_stack* checker;
+
+	while (current != NULL && current->next != NULL)
 	{
-		if (array[i] == ' ')
-			i++;
-		if (array[i] < '0' || array[i] > '9')
-			ft_error();
-		i++;
+		checker = current->next;
+		while (checker != NULL)
+		{
+			if (current->value == checker->value)
+			{
+				ft_error();
+			}
+			checker = checker->next;
+		}
+		current = current->next;
+	}
+	return 0;
+}
+
+void one_arg(char *array, t_stack **head)
+{
+	int i = 0;
+	char *token = strtok(array, " ");
+	int pos = 0;
+
+	while (token != NULL) {
+		for (i = 0; token[i] != '\0'; i++)
+		{
+			if (token[i] < '0' || token[i] > '9')
+			{
+				ft_error();
+			}
+		}
+		ft_add(head, atoi(token), pos);
+		pos++;
+		token = strtok(NULL, " ");
 	}
 }
 
-void	various_args(int ac, char **av)
-{
-	int	j;
-	int	i;
+void various_args(int ac, char **av, t_stack **head) {
+	int i;
+	int j;
+	int pos;
 
+	pos = 0;
 	i = 1;
+	j = 0;
 	while (i < ac)
 	{
-		j = 0;
 		while (av[i][j] != '\0')
 		{
-			if (av[1][j] == ' ')
+			if (av[i][j] < '0' || av[i][j] > '9')
+			{
 				ft_error();
-			else if (av[i][j] < '0' || av[i][j] > '9')
-				ft_error();
+			}
 			j++;
 		}
+		ft_add(head, ft_atoi(av[i]), pos);
+		pos++;
 		i++;
 	}
 }
 
-void	check_args(int ac, char **av)
+void	check_args(int ac, char **av, t_stack **head)
 {
 	if (ac == 2)
-		one_arg(av[1]);
+	{
+		one_arg(av[1], head);
+	}
 	else if (ac > 2)
-		various_args(ac, av);
+	{
+		various_args(ac, av, head);
+	}
 	else
+	{
 		exit(EXIT_SUCCESS);
+	}
+}
+
+int	main(int ac, char **av)
+{
+	t_stack	*head;
+
+	head = NULL;
+	check_args(ac, av, &head);
+	hasDuplicates(head);
+	return (0);
 }
