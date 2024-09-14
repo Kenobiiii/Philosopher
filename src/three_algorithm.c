bbbc8221367c7e5a0feecef4f67be10ab4e751e1
Pablo Romero Pons-Fuster <paromero@c3r9s2.42malaga.com>
2024-09-14 12:15:38 +0200

    Corregida norminette three_algorithm

--- src/three_algorithm.c
+++ src/three_algorithm.c
@@ -6,7 +6,7 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 10:45:04 by paromero          #+#    #+#             */
-/*   Updated: 2024/09/14 12:06:14 by paromero         ###   ########.fr       */
+/*   Updated: 2024/09/14 12:15:07 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,61 +14,61 @@
 
 void	ft_higher_middle(t_stack **list)
 {
-	t_stack *node1;
-	t_stack *node2;
+	t_stack	*node1;
+	t_stack	*node2;
 	t_stack	*node3;
 
 	node1 = *list;
 	node2 = node1->next;
 	node3 = node2->next;
-	if(node3->index > node2->index)
-			exit(EXIT_SUCCESS);
-		else if (node3->index < node2->index)
+	if (node3->index > node2->index)
+		exit(EXIT_SUCCESS);
+	else if (node3->index < node2->index)
+	{
+		if (node1->index < node3->index)
 		{
-			if (node1->index < node3->index)
-			{
-				rra(list);
-				sa(list);
-			}
-			else
-				rra(list);
+			rra(list);
+			sa(list);
 		}
+		else
+			rra(list);
+	}
 }
 
 void	ft_rest(t_stack **list)
 {
-	t_stack *node1;
-	t_stack *node2;
+	t_stack	*node1;
+	t_stack	*node2;
 	t_stack	*node3;
 
 	node1 = *list;
 	node2 = node1->next;
 	node3 = node2->next;
-	if(node1->index > node3->index)
-		{
-			if (node3->index > node2->index)
-				ra(list);
-			else
-			{
-				ra(list);
-				sa(list);
-			}
-		}
+	if (node1->index > node3->index)
+	{
+		if (node3->index > node2->index)
+			ra(list);
 		else
+		{
+			ra(list);
 			sa(list);
+		}
+	}
+	else
+		sa(list);
 }
 
-void    three_algorithm(t_stack **list)
+void	three_algorithm(t_stack **list)
 {
-    t_stack *node1;
-	t_stack *node2;
+	t_stack	*node1;
+	t_stack	*node2;
 	t_stack	*node3;
 
 	node1 = *list;
 	node2 = node1->next;
 	node3 = node2->next;
-	if(node2->index > node1->index)
+	if (node2->index > node1->index)
 		ft_higher_middle(list);
-	else if(node1->index > node2->index)
+	else if (node1->index > node2->index)
 		ft_rest(list);
 }
