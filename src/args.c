f417292b308ef3db51d77bbf17b523acbc1a151c
Pablo Romero Pons-Fuster <paromero@c1r12s6.42malaga.com>
2024-09-17 16:33:51 +0200

    Añadido comprobante de +

--- src/args.c
+++ src/args.c
@@ -75,7 +75,8 @@ void	one_arg(char *array, t_stack **list)
 		i = 0;
 		while (token[i] != '\0')
 		{
-			if (!ft_isdigit(token[i]) && token[i] != '-')
+			if (!ft_isdigit(token[i]) && token[i] != '-'
+				&& token[i] != '+')
 				ft_error();
 			i++;
 		}
@@ -97,7 +98,8 @@ void	various_args(int ac, char **av, t_stack **list)
 	{
 		while (av[i][j] != '\0')
 		{
-			if (!ft_isdigit(av[i][j]) && av[i][j] != '-')
+			if (!ft_isdigit(av[i][j]) && av[i][j] != '-'
+				&& av[i][j] != '+')
 			{
 				ft_error();
 			}
