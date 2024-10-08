6a3d440adf2d1a76d785dc50b386e4bdb9157d9b
Pablo Pons <pablo.romeroponsfuster@gmail.com>
2024-10-08 09:47:33 +0200

    añadidio defines de errores

---
+++ src/philo.h
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 11:37:09 by paromero          #+#    #+#             */
+/*   Updated: 2024/10/08 09:47:15 by paromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <stdbool.h>
+
+# define WRONG_INPUT 1
+# define MALLOC_ERROR 2
+
+typedef enum e_philo_state
+{
+	EATING = 0,
+	SLEEPING = 1,
+	THINKING = 2,
+	DEAD = 3,
+	FULL = 4,
+	IDLE = 5
+}	t_state;
+
+struct	s_data;
+
+typedef struct s_philo
+{
+	int				id;
+	int				nb_meals_had;
+	struct s_data	*data;
+	t_state			state;
+	pthread_mutex_t	*left_f;
+	pthread_mutex_t	*right_f;
+	pthread_mutex_t	mut_state;
+	pthread_mutex_t	mut_nb_meals_had;
+	pthread_mutex_t	mut_last_eat_time;
+	u_int64_t		last_eat_time;
+}t_philo;
+wa
+typedef struct s_data
+{
+	int				nb_philos;
+	int				nb_meals;
+	int				nb_full_p;
+	bool			keep_iterating;
+	u_int64_t		eat_time;
+	u_int64_t		die_time;
+	u_int64_t		sleep_time;
+	u_int64_t		start_time;
+	pthread_mutex_t	mut_eat_t;
+	pthread_mutex_t	mut_die_t;
+	pthread_mutex_t	mut_sleep_t;
+	pthread_mutex_t	mut_print;
+	pthread_mutex_t	mut_nb_philos;
+	pthread_mutex_t	mut_keep_iter;
+	pthread_mutex_t	mut_start_time;
+	pthread_t		monit_all_alive;
+	pthread_t		monit_all_full;
+	pthread_t		*philo_ths;
+	pthread_mutex_t	*forks;
+	t_philo			*philos;
+}t_data;
+
+#endif
