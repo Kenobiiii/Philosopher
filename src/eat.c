2cd7a89233249b4402be699e1e774fdfca220205
Pablo Pons <pablo.romeroponsfuster@gmail.com>
2024-10-11 19:02:36 +0200

    Añadido cambio de estado

--- src/eat.c
+++ src/eat.c
@@ -6,7 +6,7 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 17:53:05 by paromero          #+#    #+#             */
-/*   Updated: 2024/10/11 18:47:49 by paromero         ###   ########.fr       */
+/*   Updated: 2024/10/11 19:02:22 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	eat(t_philo	*philo)
 	printf("%llu Philosopher %d is eating",
 		get_time() - philo->data->start_time, philo->id);
 	philo->last_eat_time = (get_time() - philo->data->start_time);
+	philo->state = EATING;
 	pthread_mutex_unlock(&philo->data->forks[philo->left_f]);
 	pthread_mutex_unlock(&philo->data->forks[philo->right_f]);
 	pthread_mutex_unlock(&philo->mut_last_eat_time);
