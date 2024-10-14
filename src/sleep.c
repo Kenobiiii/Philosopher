ba30d87b43e699248e6143ade329a22e49c276db
Pablo Romero Pons-Fuster <paromero@c3r7s2.42malaga.com>
2024-10-14 11:19:07 +0200

    Creado sleep.c
    sleep.c
    	-> cambio estado del filósofo
    	-> printeo lo que hace
    	-> duerme el tiempo necesario

---
+++ src/sleep.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sleep.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 11:03:05 by paromero          #+#    #+#             */
+/*   Updated: 2024/10/14 11:18:40 by paromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	sleep(t_philo *philo)
+{
+	set_state(philo, SLEEPING);
+	printf("%llu Philosopher %d is sleeping\n",
+		get_time - philo->data->start_time, philo->id);
+	usleep(philo->data->sleep_time * 1000);
+}
