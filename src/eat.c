489c68df3edecef8b4067a7fac3c528326ff98ba
Pablo Romero Pons-Fuster <paromero@c3r7s2.42malaga.com>
2024-10-14 11:01:35 +0200

    Creada función takeforks
    eat.c
    
    	takeforks
    		-> mutea los tenedores
    		-> escribe que los ha cogido

--- src/eat.c
+++ src/eat.c
@@ -6,18 +6,27 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 17:53:05 by paromero          #+#    #+#             */
-/*   Updated: 2024/10/11 19:10:11 by paromero         ###   ########.fr       */
+/*   Updated: 2024/10/14 11:00:39 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	eat(t_philo	*philo)
+void	takeforks(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->data->forks[philo->left_f]);
+	printf("%llu Philosopher %d has taken a fork\n",
+		get_time() - philo->data->start_time, philo->id);
 	pthread_mutex_lock(&philo->data->forks[philo->right_f]);
+	printf("%llu Philosopher %d has taken a fork\n",
+		get_time() - philo->data->start_time, philo->id);
+}
+
+void	eat(t_philo	*philo)
+{
+	takeforks(philo);
 	pthread_mutex_lock(&philo->mut_last_eat_time);
-	printf("%llu Philosopher %d is eating",
+	printf("%llu Philosopher %d is eating\n",
 		get_time() - philo->data->start_time, philo->id);
 	philo->last_eat_time = (get_time() - philo->data->start_time);
 	philo->state = EATING;
