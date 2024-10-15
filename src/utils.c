6a11676817165e3a0b9a734ec80714b2947e31e3
Pablo Pons <pablo.romeroponsfuster@gmail.com>
2024-10-15 17:31:15 +0200

    quitadas func get

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 11:07:05 by paromero          #+#    #+#             */
-/*   Updated: 2024/10/14 19:25:40 by paromero         ###   ########.fr       */
+/*   Updated: 2024/10/15 17:26:31 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,6 @@ void	set_state(t_philo *philo, t_state state)
 	if (philo->state != DEAD)
 		philo->state = state;
 	pthread_mutex_unlock(&philo->mut_state);
-}
-
-t_state	get_state(t_philo *philo)
-{
-	t_state	state;
-	
-	pthread_mutex_lock(&philo->mut_state);
-	state = philo->state;
-	pthread_mutex_unlock(&philo->mut_state);
-	return (state);
-}
-
-bool	get_keep_iter(t_data *data)
-{
-	bool	keep_iter;
-
-	pthread_mutex_lock(&data->mut_keep_iter);
-	keep_iter = data->keep_iterating;
-	pthread_mutex_unlock(&data->mut_keep_iter);
-	return (keep_iter);
 }
 
 void	set_keep_iter(t_data *data, bool state)
