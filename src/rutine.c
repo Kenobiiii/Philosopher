d71deecce067a66209c91e507ffe9350b389b310
Pablo Pons <pablo.romeroponsfuster@gmail.com>
2024-10-16 19:49:53 +0200

    llamada a get_state

--- src/rutine.c
+++ src/rutine.c
@@ -6,7 +6,7 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 11:59:00 by paromero          #+#    #+#             */
-/*   Updated: 2024/10/16 12:34:12 by paromero         ###   ########.fr       */
+/*   Updated: 2024/10/16 18:25:49 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,11 @@ void	*rutine(void	*av)
 	{
 		if (eat(philo) != 0)
 			break ;
-		if (philo->state == DEAD)
+		if (get_state(philo) == DEAD)
 			break ;
 		if (ft_sleep(philo) != 0)
 			break ;
-		if (philo->state == DEAD)
+		if (get_state(philo) == DEAD)
 			break ;
 		if (think(philo) != 0)
 			break ;
